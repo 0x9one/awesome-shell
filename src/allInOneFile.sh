1be6c8a0a85f89a753a3fea2336a235dc7da6624
byeweb2 <sy47525@gmail.com>
2022-09-09 23:50:56 +0100

    Make a conditions for your life

--- src/allInOneFile.sh
+++ src/allInOneFile.sh
@@ -19,3 +19,31 @@ echo "My name is ${NAME}"
 # read -p "Message " variableName | -p mean prompt to the user 
 read -p "Enter your age: " AGE
 echo "You are $AGE years old"
+
+# Write your conditions 
+# We will use AGE variable 
+# If statement
+# if [ your condition ]
+if [ $AGE == 23 ]
+then 
+    echo You are welcome to the class
+fi
+# If-Else statement
+# We will use NAME variable
+if [ "$NAME" == "Swpacode" ] 
+then 
+    echo "You are the right persone"
+else 
+    echo "You are not the right persone. Sorry $NAME"
+fi
+# Else-If statement
+# We use NAME variable
+if [ "$NAME" == "Shell" ]
+then 
+    echo You are not $NAME 
+elif [ "$NAME" == "Byeweb2" ]
+then
+    echo "Welcome to your home"
+else 
+    echo Sorry your name not found
+fi
