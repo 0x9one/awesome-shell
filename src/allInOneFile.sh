fa0ae92cca8d263c2afe2247a72c53c802220398
byeweb2 <sy47525@gmail.com>
2022-09-09 23:20:06 +0100

    Write a variable and how to write it

--- src/allInOneFile.sh
+++ src/allInOneFile.sh
@@ -3,4 +3,14 @@
 # The first you have to write is the sharp bang with bash location form /bin/bash
 
 # Print Hello world 
+# we use -> echoo write_your_message
 echo Hello World!
+
+# Variables must be
+# Uppercase by convention
+# Letters, number, and underscores
+# variableName=value 
+NAME="Byeweb2"
+echo "My name is $NAME"
+# Or we can use 
+echo "My name is ${NAME}"
